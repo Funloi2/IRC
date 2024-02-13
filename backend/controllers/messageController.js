650a2a45bac6693ff5d21ed65dfbfef74525985b
Charles <thefunloi11@gmail.com>
2024-02-13 08:54:52 +0100

    ajout des commandes dans le backend

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,20 +1,37 @@
 const Message = require("../models/messageModel");
 const {getChannels} = require("./channelController");
 const Channel = require('../models/channelModel')
+const e = require("express");
 
 
 // Post a message on a channel
 
 const postMessage = async (req, res) => {
+    let message;
     try {
-        if(req.body.message === '/list'){
+        message = req.body.message
+        const verifCommand = message.split(' ')
+        if (verifCommand[0] === '/list') {
             const channels = await Channel.find({}).sort({createdAt: -1})
-            res.status(200).json(channels)
-        }else{
+            console.log(channels)
+        } else if (verifCommand === '/create') {
+            const channel = await Channel.create({name: req.body.message})
+            res.status(200).json(channel)
+        } else  if (verifCommand === '/delete') {
+            const channel = await Channel.findOneAndDelete({name: req.body.message})
+            res.status(200).json({msg: 'channel deleted'})
+        // } else if (verifCommand === '/update') {
+        //     const channel = await Channel.findOneAndUpdate ({name: req.body.message})
+        //     res.status(200).json(channel)
+        //
+        }else if (verifCommand[0] === '/join') {
+            // const channel = await Channel
+        }
+        else {
             const message = await Message.create(req.body);
             res.status(200).json(message);
+            console.log(message[0])
         }
-
 
 
     } catch (err) {
